707fd0cd2162565591aa3e38c226303bc6cf4d7c
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-10-24 08:32:37 +0100

    format deploy info

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,28 @@
-# config valid only for current version of Capistrano
+######################################################################
+#### Lock config to version 3.6.1
+######################################################################
 lock '3.6.1'
+
+
+######################################################################
+#### Project URL and name
+######################################################################
 
 set :application, 'painless-deployment-workflows-with-capistrano-3'
 set :repo_url, 'git@github.com:Assortment/painless-deployment-workflows-with-Capistrano-3.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
+######################################################################
+#### Deploy info
+######################################################################
+set :scm, :git
+set :use_sudo, false
 set :deploy_to, '/var/www/html/painless-deployment-workflows-with-capistrano-3'
 
-# Default value for :scm is :git
-set :scm, :git
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+######################################################################
+#### Further config
+######################################################################
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 set :keep_releases, 3
+set :log_level, :debug

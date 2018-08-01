2498cba78e5d90481679a31538e68a7f536267a5
abhay <abhay@increasingly.com>
2018-08-01 19:17:53 +0530

    setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 ######################################################################
 
 set :branch, "master"
-server "ec2-34-252-7-242.eu-west-1.compute.amazonaws.com", roles: %w{web app db}, ssh_options: { user: 'ubuntu', keys: %w(/home/inc/.ssh/aws_increasingly.pem), auth_methods: %w(publickey) }
+server "ec2-34-252-7-242.eu-west-1.compute.amazonaws.com", roles: %w{web}, ssh_options: { user: 'ubuntu', keys: %w(/home/inc/.ssh/aws_increasingly.pem), auth_methods: %w(publickey) }
 set :deploy_to, '/var/www/html/Clients/automator'
 
 ##############################

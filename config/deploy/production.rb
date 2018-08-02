863941826cb6c55910638bf1c076505850914e52
abhay <abhay@increasingly.com>
2018-08-02 11:30:44 +0530

    setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@
 ######################################################################
 
 set :branch, "master"
-server "increasingly.co", roles: %w{web}, ssh_options: { user: 'ubuntu', keys: %w(/home/inc/.ssh/aws_increasingly.pem), auth_methods: %w(publickey) }
-set :deploy_to, '/var/www/html/Clients/automator'
+server "ec2-34-252-7-242.eu-west-1.compute.amazonaws.com", roles: %w{web}, ssh_options: { user: 'ubuntu', keys: %w(/home/inc/Downloads/aws_increasingly.pem), auth_methods: %w(publickey) }
+set :deploy_to, '/var/automator'
 
 ##############################
 #### Further info
